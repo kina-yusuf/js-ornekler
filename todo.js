15071efafa94b88bfc68a4b769c2b16fa8fb3a60
yusufkina <yusufkna55@gmail.com>
2023-12-30 21:06:26 +0300

    todolist projesi eklendi js ile listeye veri ekleme, silme, tamamını silme ve filtreleme işlemleri yapıldı.

---
+++ todo.js
@@ -0,0 +1,145 @@
+// Tüm elementleri seçmek
+
+const form = document.querySelector("#todoAddForm");
+const addInput = document.querySelector("#todoName");
+const todoList = document.querySelector(".list-group");
+const firstCardBody = document.querySelectorAll(".card-body")[0];
+const secondCardBody = document.querySelectorAll(".card-body")[1];
+const clearButton = document.querySelector("#clearButton");
+const filterInput = document.querySelector("#todoSearch");
+
+let todos =[];
+
+runEvents();
+
+function runEvents(){
+    form.addEventListener("submit", addTodo);
+    document.addEventListener("DOMContentLoaded",pageLoaded);
+    secondCardBody.addEventListener("click",removeTodoToUI);
+    clearButton.addEventListener("click",allTodosEverywhere);
+    filterInput.addEventListener("keyup",filter);
+}
+
+function pageLoaded(){
+    chechTodosFromStorage();
+    todos.forEach(function(todo){
+        addTodoToUI(todo);
+    });
+}
+
+function filter(e){
+    const filterValue = e.target.value.toLowerCase().trim();
+    const todoListesi = document.querySelectorAll(".list-group-item");
+
+    if(todoListesi.length>0){
+        todoListesi.forEach(function(todo){
+            if(todo.textContent.toLowerCase().trim().includes(filterValue)){
+                todo.setAttribute("style","display : block");
+            }else{
+                todo.setAttribute("style","display : none !important");
+            }
+        });
+    }else{
+        showAlert("warning","Filtreleme yapmak için en az bir tane todo lazım...");
+    }
+}
+
+function allTodosEverywhere(){
+    const todoListesi = document.querySelectorAll(".list-group-item");
+    if(todoListesi.length>0){
+        //ekrandan silme
+        todoListesi.forEach(function(todo){
+            todo.remove();
+        });
+
+        //storage dan silme 
+        todos=[];
+        localStorage.setItem("todos",JSON.stringify(todos));
+        showAlert("success", "başarılı bir şekilde silindi");
+
+    }else{
+        showAlert("warning","silmek için en az bir tane todo lazım ");
+    }
+}
+
+function removeTodoToStorage(removeTodo){
+    chechTodosFromStorage();
+    todo.forEach(function(todo,index){
+       if(removeTodo===todo){
+            todos.splice(index,1);
+       } 
+    });
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function removeTodoToUI(e){
+    if(e.target.className==="fa fa-remove"){
+        //ekrandan silme
+        const todo = e.target.parentElement.parentElement;
+        todo.remove();
+        //storage dan silme
+        removeTodoToStorage(todo.textContent);
+        showAlert("success", "Todo başarıyla silindi.");
+    }
+}
+
+
+function addTodo(e){
+    const inputText = addInput.value.trim();
+    if(inputText==null || inputText==""){
+        showAlert("warning", "Lütfen boş geçmeyiniz.");
+    }else{
+        //Arayüze UI ekleme
+        addTodoToUI(inputText);
+        addTodoToStorage(inputText);
+        showAlert("success", "Todo eklendi.");
+    }
+
+    // storage ekleme
+    e.preventDefault();
+}
+
+function addTodoToUI(newTodo){
+    const li = document.createElement("li");
+    li.className = "list-group-item d-flex justify-content-between";
+    li.textContent = newTodo;
+
+    const a =document.createElement("a");
+    a.href= "#";
+    a.className= "delete-item";
+
+    const i = document.createElement("i");
+    i.className= "fa fa-remove";
+
+    a.appendChild(i);
+    li.appendChild(a);
+    todoList.appendChild(li);
+
+    addInput.value = "";
+}
+
+function addTodoToStorage(newTodo){
+    chechTodosFromStorage();
+    todos.push(newTodo);
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function chechTodosFromStorage(){
+    if(localStorage.getItem("todos")===null){
+        todos = [];
+    }else{
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+}
+
+function showAlert(type, message){
+    const div = document.createElement("div");
+    // div.className="alert alert-"+type;
+    div.className = `alert alert-${type}`; // litirel template kullanım şekli
+    div.textContent=message;
+    firstCardBody.appendChild(div);
+
+    setTimeout(function(){ // 2.5 saniye sonra mesaj kısmı gitmesi için...
+        div.remove();
+    },2500);
+}
